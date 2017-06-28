35c4166e4ec3979c9c81e1174861dfbef001c676
Joe Jarvis <joe.jarvis109@gmail.com>
2017-06-28 14:27:49 -0400

    Textbox is now auto-focused

--- extension/notes.js
+++ extension/notes.js
@@ -4,6 +4,11 @@ function saveNotes() {
     });
 }
 function loadNotes(e) {
+    // Needed to autofocus textbox
+    // https://stackoverflow.com/a/11400653
+    if (location.search !== "?focusHack") {
+        location.search = "?focusHack";
+    }
     browser.storage.local.get().then((res) => {
         document.getElementById("notes").value = res.general_notes || "";
     });
