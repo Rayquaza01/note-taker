7698110dcc409aa4b10e3c78f59b69ea691b8068
Joe Jarvis <joe.jarvis109@gmail.com>
2017-06-28 00:31:35 -0400

    Minor fix: changed event listener to fix extension on linux

--- extension/notes.js
+++ extension/notes.js
@@ -1,10 +1,13 @@
-document.getElementById("notes").addEventListener("change", () => {
+function saveNotes() {
     browser.storage.local.set({
         general_notes: document.getElementById("notes").value
     });
-});
-document.addEventListener("DOMContentLoaded", () => {
+}
+function loadNotes(e) {
     browser.storage.local.get().then((res) => {
         document.getElementById("notes").value = res.general_notes || "";
     });
-});
+    e.preventDefault();
+}
+document.getElementById("notes").addEventListener("input", saveNotes);
+document.addEventListener("DOMContentLoaded", loadNotes);
