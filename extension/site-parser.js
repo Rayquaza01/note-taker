3c541d7d7f655a2885c678bc5697e0716cb43887
Joe Jarvis <joe.jarvis109@gmail.com>
2017-10-13 23:05:15 -0400

    Moved site parser function to separate file for easier use

---
+++ extension/site-parser.js
@@ -0,0 +1,26 @@
+async function siteParser(rawUrl) {
+    var res = await browser.storage.local.get("options");
+    var url = new URL(rawUrl);
+    if (url.protocol === "about:") {
+        return url.protocol + url.pathname;
+    } else if (url.protocol.match(/https?:/g)) {
+        var site = psl.parse(url.hostname);
+        if (res.options.per_site === "url") {
+            return url.hostname + url.pathname;
+        } else if (res.options.subdomains_mode === "blacklist") {
+            if (res.options.subdomains.indexOf(site.domain) > -1 || res.options.subdomains.length === 0) {
+                return site.domain;
+            } else {
+                return url.hostname;
+            }
+        } else if (res.options.subdomains_mode === "whitelist") {
+            if (res.options.subdomains.indexOf(site.domain) > -1 || res.options.subdomains.length === 0) {
+                return url.hostname;
+            } else {
+                return site.domain;
+            }
+        }
+    } else {
+        return "general_notes";
+    }
+}
