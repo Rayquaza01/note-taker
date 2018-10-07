755b8537b9bd65669c8e6deb64d3fac2fede1e14
Joe Jarvis <joe.jarvis109@gmail.com>
2018-10-07 15:45:32 -0400

    Save notes, link to note taker python

--- extension/notes_native.js
+++ extension/notes_native.js
@@ -2,7 +2,8 @@ const DOM = generateElementsVariable([
     "request",
     "port",
     "import",
-    "export"
+    "export",
+    "res"
 ]);
 
 function requestPermissions() {
@@ -13,17 +14,22 @@ function requestPermissions() {
 
 async function exportNotes() {
     let res = await browser.storage.local.get(["general_notes", "site_notes"])
-    fetch("http://localhost:" + DOM.port.value + "/push", {
+    let response = await fetch("http://localhost:" + DOM.port.value + "/push", {
         method: "POST",
         headers: {
             "Content-Type": "application/json;"
         },
         body: JSON.stringify(res)
     });
+    DOM.res.innerText = await response.text();
 }
 
 async function importNotes() {
-    fetch("http://localhost:" + DOM.port.value + "/pull");
+    let response = await fetch("http://localhost:" + DOM.port.value + "/pull");
+    await browser.storage.local.remove(["general_notes", "site_notes"]);
+    let json = await response.json();
+    browser.storage.local.set(json);
+    DOM.res.innerText = JSON.stringify(json);
 }
 
 DOM.request.addEventListener("click", requestPermissions);
