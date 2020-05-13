ee44b8142b6427ef5cfc754dbc510c88516a3894
Joe Jarvis <joe.jarvis109@gmail.com>
2020-05-13 16:18:28 -0400

    Remove old files

--- extension/elements.js
+++
@@ -1,8 +0,0 @@
-/* eslint no-unused-vars: 0 */
-function generateElementsVariable(list) {
-    let dom = {};
-    for (let item of list) {
-        dom[item] = document.getElementById(item);
-    }
-    return dom;
-}
