b62afb558f739bb2c0b72c9a933f8daeaab38d42
Joe Jarvis <joe.jarvis109@gmail.com>
2023-04-24 16:09:41 -0400

    React boilerplating

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function App() {
+    return (
+        <h1>Hi</h1>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtrackPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+module.exports = (env, argv) => {
+    const config = {
+        mode: argv.mode,
+        entry: {
+            index: __dirname + "/src/index.tsx"
+        },
+        devtool: "source-map",
+        output: {
+            path: __dirname + "/dist",
+            filename: "[name].bundle.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtrackPlugin.loader, "css-loader"]
+                }
+            ]
+        },
+        resolve: {
+            extensions: [ ".ts", ".tsx", ".js", ".jsx" ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "src/index.html",
+                filename: "index.html",
+                chunks: ["index"],
+            }),
+            new copyWebpackPlugin({
+                patterns: [
+                    { from: "node_modules/webextension-polyfill/dist/browser-polyfill.min.js", to: "third-party/browser-polyfill.min.js" },
+                    { from: "node_modules/react/umd/" + (argv.mode === "production" ? "react.production.min.js" : "react.development.js"), to: "third-party/react.js"},
+                    { from: "node_modules/react-dom/umd/" + (argv.mode === "production" ? "react-dom.production.min.js" : "react-dom.development.js"), to: "third-party/react-dom.js"}
+                ]
+            })
+        ],
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDOM",
+        },
+        optimization: {
+            usedExports: true,
+            minimizer: [
+                new CssMinimizerWebpackPlugin(),
+                new TerserWebpackPlugin()
+            ]
+        }
+    }
+
+    return config;
+}
