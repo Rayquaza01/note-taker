183b6674149e7f2e53cf56774bac1f93e24bd424
Arnaught <rayquaza01@outlook.com>
2025-02-11 11:25:50 -0500

    Use statusbar in main app

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import { Drawer } from "./Components/Modal/Drawer/Drawer";
 import { Dialog } from "./Components/Modal/Dialog/Dialog";
 
 import { DrawerList } from "./Components/DrawerList";
+import { StatusBar } from "./Components/StatusBar";
+
+import { useStorageLocal } from "./Hooks/useStorage";
+
+import { Note } from "./Notes";
 
 export function App() {
     const [open, setOpen] = useState(false);
     const [openDialog, setOpenDialog] = useState(false);
+
+    const notes = useStorageLocal<Note[]>("notes", []);
+
+    const [currentNote, setCurrentNote] = useState<string>("Default");
 
     function toggleDrawer() {
         setOpen(!open);
@@ -18,15 +27,22 @@ export function App() {
         setOpenDialog(!openDialog);
     }
 
+    function newNote() {
+        notes.push({ name: "hi", value: ["ok"] })
+        browser.storage.local.set({ notes })
+    }
+
     return (
         <div>
             <button onClick={toggleDrawer}>Toggle</button>
             <button onClick={toggleDialog}>Toggle Dialog</button>
+            <StatusBar current={currentNote} onMenuClick={toggleDrawer} />
             <Drawer open={open} onModalClick={() => setOpen(false)} anchor="left">
                 <button onClick={toggleDrawer}>Toggle</button>
+                <button onClick={newNote}>New Note</button>
                 <DrawerList />
             </Drawer>
-            <Dialog open={openDialog} onModalClick={() => setOpenDialog(false)} anchor="top">
+            <Dialog open={openDialog} onModalClick={() => setOpenDialog(false)} anchor="left">
                 <button onClick={toggleDialog}>Toggle Dialog</button>
             </Dialog>
         </div>
